c7a09ac219e0e348a8586b7cac91dde993aa870f
Biswajee <roy.biswajeet161@gmail.com>
2020-05-19 11:12:46 +0530

    removed debug code

--- KMeans.c
+++ KMeans.c
@@ -66,10 +66,10 @@ void make_clusters(int img_height, int img_width, int pts) {
 void printClusterStats(int pts) {
     int i,j,gray;
     for(i=0; i<pts; i++) {
-        printf("\n{%d, %d} %d", KMeansCluster[i].__centroid.x, KMeansCluster[i].__centroid.y, KMeansCluster[i].freePosCounter);
+        // printf("\n{%d, %d} %d", KMeansCluster[i].__centroid.x, KMeansCluster[i].__centroid.y, KMeansCluster[i].freePosCounter);
         for(j=0; j< KMeansCluster[i].freePosCounter-1; j++) {
             gray = getGrayLevel(KMeansCluster[i].points[j]);
-            printf("\n%d  (%d, %d)", gray, KMeansCluster[i].points[j].x, KMeansCluster[i].points[j].y);
+            printf("%d  ", gray);
         }
     }
 }
